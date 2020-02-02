c70013cb62fd730a3853863f5b8ec5a731a98ff0
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-02 18:27:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+// Write your code here
+const        num1 = 2;
+const        num2 = 31;
+const        multiply = num1 * num2;
+const        random   =  Math.random() + 2;
+const        mod = 16 % 6;
+const        max = Math.max(10, 15, 20);
